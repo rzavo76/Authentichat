540fae4145770c326215a746e62e6df9e04d6799
Ryan Zavoral <rzavo76@ufl.edu>
2015-12-05 16:22:25 -0500

    Fixed rc4 implementation to follow the correct usage

--- rc4.cpp
+++ rc4.cpp
@@ -1,34 +1,37 @@
 #include <string>
 #include <iostream>
 
-void swap(int& x, int& y) {
-	x = x ^ y;
-	y = x ^ y;
-	x = x ^ y;
+void swap(int* x, int* y) {
+	*x = *x ^ *y;
+	*y = *x ^ *y;
+	*x = *x ^ *y;
 }
 
 char* rc4Crypt(std::string input, std::string key) {
 	char* result = new char[input.size()]; //used as a stringbuilder
-	int x, j = 0;
+	int j = 0;
+	char z = '\0';
 	int keystream[256];
 	for (int i = 0; i < 256; i++) { //place 0 to 255 in keystream
 		keystream[i] = i;
 	}
     for (int i = 0; i < 256; i++) { //swap 0 to 255 based on key
 		j = (key[i % key.size()] + keystream[i] + j) % 256;
-		swap(keystream[i], keystream[j]);
+		swap(&keystream[i], &keystream[j]);
 	}
+	j = 0;
     for (int i = 0; i < input.size(); i++) { //may have to deal with null term char
-        x = i % 256;
+        int x = i % 256;
         j = (keystream[x] + j) % 256;
-        swap(keystream[x], keystream[j]);
-        result[i] = (char)(input[i] ^ keystream[(keystream[x] + keystream[j]) % 256]); //apply keystream
+        swap(&keystream[x], &keystream[j]);
+        z = keystream[(keystream[x] + keystream[j]) % 256];
+        result[i] = (char)(input[i] ^ z); //apply keystream
 	}
 	return result;
 }
 
 int main() {
-	char* cypheredText = rc4Crypt("AuthentichatKey", "T0uC(@#N");
+	char* cypheredText = rc4Crypt("Hello Encrypted World!", "T0uC(@#N");
 	std::cout << cypheredText << std::endl;
 	char* deCypheredText = rc4Crypt(cypheredText, "T0uC(@#N");
 	std::cout << deCypheredText << std::endl;
